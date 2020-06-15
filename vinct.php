80449b25f3aca49a0b64aa4585f3ba94f11b8f67
kevinct9 <64075226+kevinct9@users.noreply.github.com>
2020-06-16 03:29:31 +0700

    Update vinct.php

--- vinct.php
+++ vinct.php
@@ -19,7 +19,7 @@ if($pilihy == "y" || $pilihy == "sudah")
 echo "\e[96m▬▬▬▬▬pilih y atau sudah▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n";
 	goto login;
 login:
-echo "\e[96m▬▬▬ LANGSUNG KLIK ENTER DI BAWAH INI  ▬▬▬▬▬\n";
+echo "\e[96m▬▬▬    ENTER    ▬▬▬▬▬\n";
 $pass =  ( '║ User Login : ' );
 echo "\033[30;40m";
 $user = trim(fgets(STDIN));
