7c7b11a250e7772c09c9935e611dbcefb2ea90bc
kevinct9 <64075226+kevinct9@users.noreply.github.com>
2020-06-23 02:09:29 +0700

    Update function.php

--- function.php
+++ function.php
@@ -11,7 +11,7 @@ $header[] = "X-AppVersion: 3.30.2";
 $header[] = "X-UniqueId: ".time()."57".mt_rand(1000,9999);
 $header[] = "Connection: keep-alive";
 $header[] = "X-User-Locale: id_ID";
-$header[] = "X-Location: -6.933282,107.604888";
+$header[] = "X-Location: -6.942313,107.610810";
 $header[] = "X-Location-Accuracy: 3.0";
 if ($pin):
 $header[] = "pin: $pin";
